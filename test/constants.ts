ba28e0ab6abbdea691d6a733df3b3d8aad8bcb55
MaisonT <98812333+MaisonT@users.noreply.github.com>
2022-02-04 18:08:19 +0200

    Update constants.ts

--- test/constants.ts
+++ test/constants.ts
@@ -1,3 +1,3 @@
-export const xMerlin = "0xfddfE525054efaAD204600d00CA86ADb1Cc2ea8a";
-export const xCalibur = "0xfB3485c2e209A5cfBDC1447674256578f1A80eE3";
-export const LothricFin = "0xa18807e5B5C297e7aD5C254f6288f336Fe0972De";
+export const xMerlin = "0xc569dcA318F17Cfbbc4992117cE3bbFA63cF74B0";
+export const xCalibur = "0x3DCf398AD81dDC9f8Fc47f93107b1DD650Fd6DD4";
+export const LothricFin = "0xb207D37571bD6F7D7f3b274d1fA3298B289dD36b";
