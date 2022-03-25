8a3288d0e43e96212307fa080c40e19e1792a9eb
rnkq <art@nereus.finance>
2022-03-25 16:02:52 +0200

    feat: update permission

---
+++ contracts/PermissionManager.sol
@@ -0,0 +1,45 @@
+pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
+
+import "@boringcrypto/boring-solidity/contracts/BoringOwnable.sol";
+
+contract PermissionManager is BoringOwnable {
+    struct PermissionInfo {
+        uint index;
+        bool isAllowed;
+    }
+    mapping(address => PermissionInfo) public info;
+    address[] public allowedAccounts;
+
+    function permit(address _account) public onlyOwner {
+        if (info[_account].isAllowed) {
+            revert("Account is already permitted");
+        }
+        info[_account] = PermissionInfo({index: allowedAccounts.length, isAllowed: true});
+        allowedAccounts.push(_account);
+    }
+
+    function revoke(address _account) public onlyOwner {
+        PermissionInfo memory accountInfo = info[_account];
+
+        if (accountInfo.index != allowedAccounts.length-1) {
+            address last = allowedAccounts[allowedAccounts.length-1];
+            PermissionInfo storage infoLast = info[last];
+
+            allowedAccounts[accountInfo.index] = last;
+            infoLast.index = accountInfo.index;
+        }
+
+        delete info[_account];
+        allowedAccounts.pop();
+    }
+
+    function getAllAccounts() public returns (address[] memory) {
+        return allowedAccounts;
+    }
+
+    modifier isAllowed() {
+        require(info[msg.sender].isAllowed, "sender is not allowed");
+        _;
+    }
+}
