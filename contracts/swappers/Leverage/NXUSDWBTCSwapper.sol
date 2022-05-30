4b18b13a44a02e0a249e08a8da41e206a95e1d55
AntonHladkiy <105917252+Caeltrial@users.noreply.github.com>
2022-05-30 10:45:55 +0300

    fix: fixed bentobox address

--- contracts/swappers/Leverage/NXUSDWBTCSwapper.sol
+++ contracts/swappers/Leverage/NXUSDWBTCSwapper.sol
@@ -3,10 +3,33 @@ pragma solidity 0.8.9;
 
 import "@sushiswap/core/contracts/uniswapv2/interfaces/IUniswapV2Factory.sol";
 import "@sushiswap/core/contracts/uniswapv2/interfaces/IUniswapV2Pair.sol";
-import "../../interfaces/ISwapperGeneric.sol";
 
 interface Zap {
     function exchange_underlying(address _pool, int128 i, int128 j, uint256 dx, uint256 min_dy, address receiver) external returns (uint256);
+}
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    /// @notice EIP 2612
+    function permit(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
 }
 
 interface IBentoBoxV1 {
@@ -28,7 +51,7 @@ interface IBentoBoxV1 {
 }
 
 contract NXUSDWBTCSwapper {
-    IBentoBoxV1 public constant DEGENBOX = IBentoBoxV1(0x3c4479f3274113dd44F770632cC89F4AdDf33617);
+    IBentoBoxV1 public constant DEGENBOX = IBentoBoxV1(0x0B1F9C2211F77Ec3Fa2719671c5646cf6e59B775);
 
     address public constant NXUSD3POOL = 0x6BF6fc7EaF84174bb7e1610Efd865f0eBD2AA96D;
     Zap public constant ZAP3POOL = Zap(0x001E3BA199B4FF4B5B6e97aCD96daFC0E2e4156e);
