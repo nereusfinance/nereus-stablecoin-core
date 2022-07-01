9d0ed90dfc0d559f6b98b04b34c67a2f45f1ee55
vsnereus <vitsa@nereus.finance>
2022-07-01 17:56:16 +0300

    update oracle price and call updateExchangeRate

--- tasks/mockOraclePrice.ts
+++ tasks/mockOraclePrice.ts
@@ -1,18 +1,18 @@
 import { task } from "hardhat/config";
 
 task("mock-oracle-price", "Mock oracle price")
-  .addParam("asset", "Possible values: AVAXOracle, ETHOracle, BTCOracle, DAIOracle")
+  .addParam("asset", "Possible values: AVAX, ETH, BTC, DAI")
   .setAction(async ({ asset }, { run, ethers, deployments, artifacts }) => {
     await run("compile");
-    const deployment = await deployments.get(asset);
-    if (!deployment) {
-      throw Error("Deployment not found");
-    }
-    const artifact = await artifacts.readArtifact(asset);
-    if (!artifact) {
-      throw Error("Artifact not found");
-    }
+    const deployment = await deployments.get(`${asset}Oracle`);
+    const artifact = await artifacts.readArtifact(`${asset}Oracle`);
     console.log("updating...", deployment.address);
     await ethers.provider.send("hardhat_setCode", [deployment.address, artifact.deployedBytecode]);
+
+    const deploymentCauldron = await deployments.get(`${asset}Cauldron`);
+    const cauldronFactory = await ethers.getContractFactory("CauldronV2");
+    const contractCauldron = await cauldronFactory.attach(deploymentCauldron.address);
+    await contractCauldron.updateExchangeRate();
+
     await ethers.provider.send("evm_mine", []);
   });
