feb7574752f89cdb631f06ae35cca9dbb70d6f1a
vsnereus <vitsa@nereus.finance>
2022-07-28 11:06:16 +0300

    update oracle price and call updateExchangeRate

--- tasks/mockOraclePrice.ts
+++ tasks/mockOraclePrice.ts
@@ -9,11 +9,12 @@ task("mock-oracle-price", "Mock oracle price")
     console.log("updating...", deployment.address);
     await ethers.provider.send("hardhat_setCode", [deployment.address, artifact.deployedBytecode]);
 
-    const overriveCauldronPrefix = {
+    const overrideCauldronPrefix = {
       ETH: "WETH",
+      BTC: "WBTC",
     };
 
-    const deploymentCauldron = await deployments.get(`${overriveCauldronPrefix[asset] ? overriveCauldronPrefix[asset] : asset}Cauldron`);
+    const deploymentCauldron = await deployments.get(`${overrideCauldronPrefix[asset] ? overrideCauldronPrefix[asset] : asset}Cauldron`);
     const cauldronFactory = await ethers.getContractFactory("CauldronV2");
     const contractCauldron = await cauldronFactory.attach(deploymentCauldron.address);
     await contractCauldron.updateExchangeRate();
